bf71f15e245b8e15cd02b65800fe6b0c4aae8aed
Adam Rafiandri <rafiandria23@gmail.com>
2021-05-09 02:59:43 +0700

    Implemented authenticator.

---
+++ middlewares/authenticator.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const createError = require('http-errors');
+const jwt = require('jsonwebtoken');
+
+const { User, Admin } = require('../models');
+
+class Authenticator {
+  static async user(req, res, next) {
+    try {
+      const accessToken =
+        req.cookies.accessToken ||
+        req.headers.authorization?.split(' ')[1] ||
+        req.headers['X-ACCESS-TOKEN'] ||
+        req.headers['x-access-token'] ||
+        req.body.accessToken;
+
+      if (!accessToken) {
+        throw createError(401, {
+          message: 'You must be logged in!',
+        });
+      }
+
+      const { email, role } = jwt.verify(
+        accessToken,
+        process.env.JWT_ACCESS_SECRET,
+      );
+
+      switch (role) {
+        case 'user':
+          const foundUser = await User.findOne({
+            where: {
+              email,
+            },
+            attributes: {
+              exclude: ['password'],
+            },
+          });
+          req.user = foundUser;
+          break;
+
+        case 'admin':
+          const foundAdmin = await Admin.findOne({
+            where: {
+              email,
+            },
+            attributes: {
+              exclude: ['password'],
+            },
+          });
+          req.admin = foundAdmin;
+          break;
+
+        default:
+          throw createError(401, {
+            message: 'Invalid access token!',
+          });
+      }
+
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  static async admin(req, res, next) {
+    try {
+      const accessToken =
+        req.cookies.accessToken ||
+        req.headers.authorization?.split(' ')[1] ||
+        req.headers['X-ACCESS-TOKEN'] ||
+        req.headers['x-access-token'] ||
+        req.body.accessToken;
+
+      if (!accessToken) {
+        throw createError(401, {
+          message: 'You must be logged in!',
+        });
+      }
+
+      const { email, role } = jwt.verify(
+        accessToken,
+        process.env.JWT_ACCESS_SECRET,
+      );
+
+      if (role === 'admin') {
+        const foundAdmin = await Admin.findOne({
+          where: {
+            email,
+          },
+          attributes: {
+            exclude: ['password'],
+          },
+        });
+        req.admin = foundAdmin;
+      } else {
+        throw createError(401, {
+          message: 'Invalid access token!',
+        });
+      }
+
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  }
+}
+
+module.exports = Authenticator;
